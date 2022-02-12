62c6ecab555b0186d386c35c5f55a2506609fabe
haku <even2309051@gmail.com>
2022-02-12 16:52:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SAProject
+
+Developed with Unreal Engine 4
---
+++ Source/SAProject/SAProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SAProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAPROJECT_API ASAProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
